2aeb1a26386dcd0f765ea5878f07c38df6a319d7
sanad-bhowmik <sanadbhowmik93@gmail.com>
2023-06-06 21:53:56 +0600

    nav page design done

---
+++ src/Shared/Header/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from '../../../src/assets/image/logo.png';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className="navbar bg-gradient-to-r from-teal-200 to-teal-400">
+            <div className="flex-1">
+                <Link className=""><img className='h-16 w-30' src={logo} alt="" /></Link>
+            </div>
+            <div className="flex gap-6 font-serif">
+                <div className="form-control ">
+                    <h1>Home</h1>
+                </div>
+                <div className="form-control ">
+                    <h1>Instructors</h1>
+                </div>
+                <div className="form-control ">
+                    <h1>Classes</h1>
+                </div>
+                <div className="form-control ">
+                    <h1>Dashboard</h1>
+                </div>
+                <div className="dropdown dropdown-end">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="" />
+                        </div>
+                    </label>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
